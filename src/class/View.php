d9aaf8cf62e761fae0b9a767799a55a919ac6aeb
kamkams <55581846+kamkams@users.noreply.github.com>
2020-02-24 08:30:54 +0200

    undoviewtasks

--- src/class/View.php
+++ src/class/View.php
@@ -10,22 +10,6 @@ class View {
     public function printTasks($tasks)
     {
         echo "<hr>Print Tasks View</br>";
-        $sql = "SELECT*FROM todolist";
-$result = $conn->query($sql);
-
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "<div class='todo-cont'>";
-        echo "<span class='todo-cell'>id: " . $row["id"] . "</span>";
-        echo "<span class='todo-cell'>tasks: " . $row["tasks"] . "</span>";
-        echo "<span class='todo-cell'>created: " . $row["created"] . "</span>";
-        echo "<span class='todo-cell'>ptodo: " . $row["ptodo"] . "</span>";
-        echo "</div>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
+        
     }
 }
