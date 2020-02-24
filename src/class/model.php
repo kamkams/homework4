8e30ef4ed5f6d8e73c7f00c8fad748c029354a9c
kamkams <55581846+kamkams@users.noreply.github.com>
2020-02-24 08:21:19 +0200

    check model up

--- src/class/model.php
+++ src/class/model.php
@@ -24,23 +24,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-$sql = "SELECT*FROM todolist";
-$result = $conn->query($sql);
+// $sql = "SELECT*FROM todolist";
+// $result = $conn->query($sql);
 
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "<div class='todo-cont'>";
-        echo "<span class='todo-cell'>id: " . $row["id"] . "</span>";
-        echo "<span class='todo-cell'>tasks: " . $row["tasks"] . "</span>";
-        echo "<span class='todo-cell'>created: " . $row["created"] . "</span>";
-        echo "<span class='todo-cell'>ptodo: " . $row["ptodo"] . "</span>";
-        echo "</div>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         echo "<div class='todo-cont'>";
+//         echo "<span class='todo-cell'>id: " . $row["id"] . "</span>";
+//         echo "<span class='todo-cell'>tasks: " . $row["tasks"] . "</span>";
+//         echo "<span class='todo-cell'>created: " . $row["created"] . "</span>";
+//         echo "<span class='todo-cell'>ptodo: " . $row["ptodo"] . "</span>";
+//         echo "</div>";
+//     }
+// } else {
+//     echo "0 results";
+// }
+// $conn->close();
 
 
 }
