1e45c806b61ded146ddbb3c3573a8b31e35086ae
kamkams <55581846+kamkams@users.noreply.github.com>
2020-02-24 08:51:41 +0200

    render

--- src/class/View.php
+++ src/class/View.php
@@ -1,12 +1,12 @@
 <?php
 
 class View {
-    // public function render()
-    // {
-    //     require_once "../template/head.php";
-    //     require_once "../template/header.php";
-    //     require_once "../template/footer.php";
-    // }
+    public function render()
+    {
+        require_once "../template/head.php";
+        require_once "../template/header.php";
+        require_once "../template/footer.php";
+    }
     public function printTasks($tasks)
     {
         echo "<hr>Print Tasks View</br>";
