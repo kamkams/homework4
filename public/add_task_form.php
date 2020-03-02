8f8e5e00386a0e2924a8a6ea1811c2cff4ea90ba
kamkams <55581846+kamkams@users.noreply.github.com>
2020-03-02 10:18:37 +0200

    delete

--- public/add_task_form.php
+++ public/add_task_form.php
@@ -1,6 +1,6 @@
 <form class="form" action= "form.php" method = "post">
     <label for= "textinput"> New Todo:   </label>
-    <input type= "text" name="my_text" id="textinput" placeholder="Your new Todo here..">
+    <input type= "text" required name="my_text" id="textinput" placeholder="Your new Todo here..">
     <label for= "datetimegroup">Schedule time:  </label>
     <input type= "datetime-local" name="pldatetime" id="datetimegroup">
     <button type= "submit" id="add">add Todo</button>
--- public/delete.php
+++ public/delete.php
@@ -6,8 +6,3 @@ $model = new Model($config);
 $model->deleteTodo(1, $_POST["taskid"]);
 header('Location: http://localhost:8888/public/', TRUE, 302);
 
-// $model = new Model(); 
-// $model->updateTodo(1, $_POST["updateBtn"]);
-// header('Location: http://localhost:8888/public/', TRUE, 302);
-  
-
